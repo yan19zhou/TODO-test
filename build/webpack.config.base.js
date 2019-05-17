c16f5051f9e30687cb74ce6b6c6eb5193dbb2730
yan19zhou <zhouyanshengwei@163.com>
2019-05-17 10:10:44 +0800

    first commit

---
+++ build/webpack.config.base.js
@@ -0,0 +1,39 @@
+const path = require('path')
+const webpack = require('webpack')
+
+const config = {
+    target: 'web',
+    entry: path.join(__dirname, '../client/index.js'),
+    output: {
+        filename: 'bundle.[hash:8].js',
+        path: path.join(__dirname, 'dist')
+    },
+    module: {
+        rules: [{
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            {
+                test: /\.jsx$/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /mode_modules/
+            },
+            {
+                test: /\.(gif|jpg|jpeg|png|svg)$/,
+                use: [{
+                    loader: 'url-loader',
+                    options: {
+                        limit: 1024,
+                        name: 'resources/[path][name].[hash:8].[ext]'
+                    }
+                }]
+            }
+        ]
+    }
+}
+
+module.exports = config
---
+++ build/webpack.config.client.js
@@ -0,0 +1,94 @@
+const path = require('path')
+const webpack = require('webpack')
+const merge = require('webpack-merge')
+const ExtractPlugin = require('extract-text-webpack-plugin')
+const HTMLPlugin = require('html-webpack-plugin')
+const baseConfig = require('./webpack.config.base')
+
+const isDev = process.env.NODE_ENV === 'development'
+
+let config
+const devServer = {
+    port: 8000,
+    host: '0.0.0.0',
+    overlay: {
+        errors: true,
+    },
+    hot: true
+}
+const defaultPluins = [
+    new webpack.DefinePlugin({
+        'process.env': {
+            NODE_ENV: isDev ? '"development"' : '"production"'
+        }
+    }),
+    new HTMLPlugin()
+]
+
+if (isDev) {
+    config = merge(baseConfig, {
+        module: {
+            rules: [{
+                test: /\.styl/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            sourceMap: true,
+                        }
+                    },
+                    'stylus-loader'
+                ]
+            }]
+        },
+        devtool: '#cheap-module-eval-source-map',
+        devServer,
+        plugins: [...defaultPluins,
+            new webpack.HotModuleReplacementPlugin(),
+            new webpack.NoEmitOnErrorsPlugin()
+        ]
+    })
+} else {
+
+    config = merge(baseConfig, {
+        entry: {
+            app: path.join(__dirname, '../client/index.js'),
+            vendor: ['vue']
+        },
+        output: {
+            filename: '[path][name].[chunkhash:8].js'
+        },
+        module: {
+            rules: [{
+                test: /\.styl/,
+                use: ExtractPlugin.extract({
+                    fallback: 'style-loader',
+                    use: [
+                        'css-loader',
+                        {
+                            loader: 'postcss-loader',
+                            options: {
+                                sourceMap: true,
+                            }
+                        },
+                        'stylus-loader'
+                    ]
+                })
+            }]
+        },
+        plugins: [
+            ...defaultPluins,
+            new ExtractPlugin('styles.[contentHash:8].css'),
+            new webpack.optimize.CommonsChunkPlugin({
+                name: 'vendor'
+            }),
+            new webpack.optimize.CommonsChunkPlugin({
+                name: 'runtime'
+            })
+        ]
+    })
+}
+
+module.exports = config
